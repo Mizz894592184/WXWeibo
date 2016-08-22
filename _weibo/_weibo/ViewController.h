8cbd3a2c735709fd7d9a772902dc661710e1bf88
Mizz894592184 <894592184@qq.com>
2016-08-22 12:47:29 +0800

    kgkhg

---
+++ _weibo/_weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  _weibo
+//
+//  Created by mac on 16/8/22.
+//  Copyright © 2016年 LiuChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ _weibo/_weiboTests/_weiboTests.m
@@ -0,0 +1,39 @@
+//
+//  _weiboTests.m
+//  _weiboTests
+//
+//  Created by mac on 16/8/22.
+//  Copyright © 2016年 LiuChao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _weiboTests : XCTestCase
+
+@end
+
+@implementation _weiboTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
